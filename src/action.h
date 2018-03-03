379bfd2479e78e58ff2f107b1631626f4ee39fbc
Jianhui Zhao <jianhuizhao329@gmail.com>
2018-03-03 17:33:49 +0800

    change license to LGPL2
    Signed-off-by: Jianhui Zhao <jianhuizhao329@gmail.com>

--- src/action.h
+++ src/action.h
@@ -1,22 +1,20 @@
 /*
- * The Action handler is a simple libuhttpd handler that processes requests
- * by invoking registered C functions. The action handler is ideal for
- * situations when you want to generate a simple response using C code. 
- *
  * Copyright (C) 2017 Jianhui Zhao <jianhuizhao329@gmail.com>
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
  */
 
 #ifndef _ACTION_H
