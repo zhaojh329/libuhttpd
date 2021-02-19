f34f8cbb8ad0a0258292a7e3fa16c2b90751b847
Jianhui Zhao <zhaojh329@gmail.com>
2021-02-19 15:06:33 +0800

    Improve gen_cert.sh
    Signed-off-by: Jianhui Zhao <zhaojh329@gmail.com>

--- gen_cert.sh
+++ gen_cert.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openssl req -x509 -newkey rsa:2048 -nodes -keyout server-key.pem -out server-cert.pem -config openssl.cnf
+openssl req -x509 -newkey rsa:2048 -nodes -keyout server-key.pem -out server-cert.pem -subj "/C=CZ/O=Acme Inc./OU=ACME/CN=ACME-DEV-123"
